7796c6eb992aabc3b06a991e0d25014d2a20eca7
Hieu Nguyen <hieu21nt@gmail.com>
2023-03-08 11:19:17 +0700

    update

--- app.vue
+++ app.vue
@@ -2,28 +2,22 @@
     <div>
         <button class="button is-primary" @click="signIn">Sign In</button>
         <button class="button is-danger" @click="signOut">Sign Out</button>
+
+        <pre>{{ credentials }}</pre>
     </div>
 </template>
 
 <script setup>
 console.log('---App---');
+const credentials = ref(null)
 
 async function signIn() {
     const email = 'test1@gmail.com'
     const password = '123456'
-    const credentials = await signInUser(email, password)
-    console.log(credentials);
+    credentials.value = await signInUser(email, password)
 }
 
 async function signOut() {
-    const result = await signOutUser()
-    console.log('result', result);
+    credentials.value = await signOutUser()
 }
-
-onMounted(async() => {
-    // const credentials = await createUser(email, password)
-    // const credentials = await signInUser(email, password)
-    // const credentials = await signOutUser()
-    // console.log(credentials);
-});
 </script>
